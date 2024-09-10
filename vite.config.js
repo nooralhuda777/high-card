c204b2074c81beec08bc0ded0362b11c1d954b4a
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-09-10 12:37:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/high-card-3.2/",
+  base: "/high-card/",
 });
